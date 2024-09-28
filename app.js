414988c249235cb32edfbd8ea3f7c4617a275eae
jramirez <jramez54@gmail.com>
2024-09-28 16:36:58 -0500

    cambios git actions

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const port = 3000;
 app.get('/', (req, res) => {
     res.send('¡Hola Mundo!');
 });
-
 app.listen(port, () => {
     console.log(`Servidor corriendo en http://localhost:${port}`);
 });
